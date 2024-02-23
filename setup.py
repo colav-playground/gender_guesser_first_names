99ff130daab199e313886aff1e4f9a8e5a6848d7
restrepo <restrepo@udea.edu.co>
2024-02-23 13:37:06 -0300

    lc

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def main():
         name="gender_guesser_first_names",
 
         # Version number (initial):
-        version="0.0.8",
+        version="0.0.9",
 
         # Application author details:
         author="restrepo",
